424c90869faa6d4a17215c2a25e0be9a74363f82
Edu Caubilla <educaubilla@gmail.com>
2025-02-04 09:31:54 +0100

    Initial Commit

---
+++ TheDoers/TheDoersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheDoersApp.swift
+//  TheDoers
+//
+//  Created by Edu Caubilla on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TheDoersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
